5899c94ca1ae7c3ae4b04c710187086a88b922e2
Victor Moreira <vmoreira@mimic.com.br>
2022-06-15 22:42:25 -0300

    feat: first version of roll a ball

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector3 offset;
+    public GameObject player;
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 offset = new Vector3(x, 0, z);
+
+        transform.position = transform.position + (offset * speed * Time.deltaTime);
+    }
+    private void FixedUpdate() {
+
+    }
+}
